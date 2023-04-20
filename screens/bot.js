6db1557361330c9b453cad932cdff99db84b0fc1
ObaidaHY <obaidah@mail.tau.ac.il>
2023-04-20 05:12:45 +0300

    using oneai Html-to-article pipeline

--- screens/bot.js
+++ screens/bot.js
@@ -3,15 +3,16 @@ import { OneAI } from 'oneai';
 const oneai = new OneAI('4e176a0e-a844-4060-a4fe-4a17e7ecdf56');
 
 const pipeline = new oneai.Pipeline(
+   oneai.skills.htmlToArticle(),
    oneai.skills.summarize()
 );
 /*oneai.skills.names(),
    oneai.skills.summarize(),
    oneai.skills.highlights()*/
 
-const output = await pipeline.run('Whether to power translation to document summarization, enterprises are increasing their investments in natural language processing (NLP) technologies. According to a 2021 survey from John Snow Labs and Gradient Flow, 60% of tech leaders indicated that their NLP budgets grew by at least 10% compared to 2020, while a third said that spending climbed by more than 30%');
+/*const output = await pipeline.run('Whether to power translation to document summarization, enterprises are increasing their investments in natural language processing (NLP) technologies. According to a 2021 survey from John Snow Labs and Gradient Flow, 60% of tech leaders indicated that their NLP budgets grew by at least 10% compared to 2020, while a third said that spending climbed by more than 30%');
 
-console.log(output.summary.text);
+console.log(output.summary.text);*/
 
 /*
 import axios from 'axios';
@@ -82,7 +83,7 @@ async function getArticles() {
   return fullArticles;
 }
 
-var articles = await getArticles()
+/*var articles = await getArticles()
 
 //console.log(articles);
 //console.log("length :", articles.length);
@@ -90,11 +91,22 @@ var articles = await getArticles()
 var content_pipe = await pipeline.run(articles[6]);
 //console.log(articles[0])
 //console.log(content_pipe);
-console.log(content_pipe.summary.text);
+console.log(content_pipe.summary.text);*/
 
 
 
 
+const topHeadlines = await newsapi.v2.topHeadlines({
+  country: 'us',
+  category: 'general',
+  pageSize: 10
+});
+const articles = topHeadlines.articles;
+for (let i = 0; i < 1; i++) {
+    var content_pipe = await pipeline.run(articles[i].url);
+    console.log(content_pipe.htmlArticle.summary.text);
+    console.log()
+}
 
 
 
