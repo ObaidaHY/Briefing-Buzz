7fafc6ada9898b43cad3cd818ad36324e7ec7e72
ObaidaHY <obaidah@mail.tau.ac.il>
2023-04-22 02:29:21 +0300

    after checkout, check bot again

--- screens/bot.js
+++ screens/bot.js
@@ -3,15 +3,16 @@ import { OneAI } from 'oneai';
 const oneai = new OneAI('4e176a0e-a844-4060-a4fe-4a17e7ecdf56');
 
 const pipeline = new oneai.Pipeline(
+   oneai.skills.htmlToArticle(),
    oneai.skills.summarize()
 );
 /*oneai.skills.names(),
    oneai.skills.summarize(),
    oneai.skills.highlights()*/
 
-const output = await pipeline.run('Whether to power translation to document summarization, enterprises are increasing their investments in natural language processing (NLP) technologies. According to a 2021 survey from John Snow Labs and Gradient Flow, 60% of tech leaders indicated that their NLP budgets grew by at least 10% compared to 2020, while a third said that spending climbed by more than 30%');
+/*const output = await pipeline.run('Whether to power translation to document summarization, enterprises are increasing their investments in natural language processing (NLP) technologies. According to a 2021 survey from John Snow Labs and Gradient Flow, 60% of tech leaders indicated that their NLP budgets grew by at least 10% compared to 2020, while a third said that spending climbed by more than 30%');
 
-console.log(output.summary.text);
+console.log(output.summary.text);*/
 
 /*
 import axios from 'axios';
@@ -51,8 +52,6 @@ import NewsAPI from 'newsapi';
 import cheerio from 'cheerio'
 //const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('206b9ac974c74f53b9f44291c63eaeed');
-const apiKey = "206b9ac974c74f53b9f44291c63eaeed";
-const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
 /*fetch(url)
   .then(response => response.json())
@@ -65,13 +64,14 @@ const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 async function getArticles() {
   const topHeadlines = await newsapi.v2.topHeadlines({
     country: 'us',
-    category: 'general'
+    category: 'general',
+    pageSize: 20
   });
 
   const articles = topHeadlines.articles;
   const fullArticles = [];
 
-  for (let i = 0; i < Math.min(articles.length, 5); i++) {
+  for (let i = 0; i < Math.min(articles.length, 10); i++) {
     const article = articles[i];
     const response = await fetch(article.url);
     const html = await response.text();
@@ -83,12 +83,30 @@ async function getArticles() {
   return fullArticles;
 }
 
-var articles = await getArticles()
+/*var articles = await getArticles()
 
-//console.log(articles[0]);
-var content_pipe = await pipeline.run(articles[0]);
-console.log(content_pipe);
-console.log(content_pipe.summary.text);
+//console.log(articles);
+//console.log("length :", articles.length);
+//console.log(articles[7])
+var content_pipe = await pipeline.run(articles[6]);
+//console.log(articles[0])
+//console.log(content_pipe);
+console.log(content_pipe.summary.text);*/
+
+
+
+
+const topHeadlines = await newsapi.v2.topHeadlines({
+  country: 'us',
+  category: 'general',
+  pageSize: 10
+});
+const articles = topHeadlines.articles;
+for (let i = 0; i < 1; i++) {
+    var content_pipe = await pipeline.run(articles[i].url);
+    console.log(content_pipe.htmlArticle.summary.text);
+    console.log()
+}
 
 
 
