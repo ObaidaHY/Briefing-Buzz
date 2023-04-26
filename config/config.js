cdfeb830ad8a8e583a73b77f61ac547991c9a179
ObaidaHY <obaidah@mail.tau.ac.il>
2023-04-27 01:58:23 +0300

    ignored

--- config/config.js
+++
@@ -1,7 +0,0 @@
-export const API_KEY = ``;
-export const endpoint = `https://newsapi.org/v2/top-headlines`;
-export const category = 'general';
-export const country = 'us';
-export const NEWS_API_KEY='';
-export const MEANINGCLOUD_API_KEY='';
-export const ONEAI_API_KEY = "";
